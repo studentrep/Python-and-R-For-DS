ba548027360242f597d1f9599f845504292ab5ce
studentrep <tug05235@temple.edu>
2021-11-07 11:41:22 +0100

    organizational changes and updating artworks_api.py code to account for nested dictionary problem

--- artworks_api.py
+++ artworks_api.py
@@ -20,6 +20,22 @@ for i in range(1,101):
     
     #for each key-value pair in the data dictionary (the data dictionary is the associated value of the "data" key)
     for eachItem in dictRes.get("data"):
+        #if the thumbnail dictionary exists
+        if eachItem["thumbnail"] != None:
+            #for each key in the thumbnail dictionary
+            for thumbnailItemKey in eachItem.get("thumbnail"):
+                #add each key-value pair in the thumbnail dictionary to the main data dictionary
+                eachItem["thumbnail." + thumbnailItemKey] = eachItem.get("thumbnail").get((thumbnailItemKey))
+            #remove the thumbnail dictionary from the main dictionary because it's repetitive information
+            eachItem.pop("thumbnail")
+        #if the color dictionary exists
+        if eachItem["color"] != None:
+            #for each key in the color dictionary
+            for colorItemKey in eachItem.get("color"):
+                #add each key-value pair in the color dictionary to the main data dictionary
+                eachItem["color." + colorItemKey] = eachItem.get("color").get((colorItemKey))
+            #remove the color dictionary from the main dictionary because it's repetitive information
+            eachItem.pop("color")
         #add the dictionary to the list we defined above
         listOfArtworks.append(eachItem)
 
